569b8306590240da16be86aae2c70a9ed9e9fb9b
reaper <virgo30892@gmail.com>
2018-12-06 17:37:09 +0100

    Added doc folder

---
+++ doc/factorviz.R
@@ -0,0 +1,10 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval=F-------------------------------------------------------------
+#  install.packages("devtools")
+#  devtools::install_github("lutsik/FactorViz")
+
